41fd7e23682ed34ee25dd2b20c62827c43944916
HeitorDalla <heitorvillavilla@gmail.com>
2026-02-26 16:58:20 -0300

    feat: adicionando classes de estudo para o spring boot

---
+++ src/main/java/com/heitor/app/Aplicacao.java
@@ -0,0 +1,10 @@
+package com.heitor.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Aplicacao {
+    public void teste () {
+        System.out.println("teste spring");
+    }
+}

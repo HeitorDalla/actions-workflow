365e15a78ab262939e02f89194e663c6c2be76c1
HeitorDalla <heitorvillavilla@gmail.com>
2026-03-18 17:01:09 -0300

    feat: adicionar handler global para as entidades

--- src/main/java/com/heitor/app/handler/GlobalNotFoundHandler.java
+++ src/main/java/com/heitor/app/handler/GlobalNotFoundHandler.java
@@ -1,7 +1,6 @@
 package com.heitor.app.handler;
 
-import com.heitor.app.exception.BookNotFoundException;
-import com.heitor.app.exception.UserNotFoundException;
+import com.heitor.app.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,4 +20,23 @@ public class GlobalNotFoundHandler {
     public String bookNotFoundHandler(BookNotFoundException book) {
         return book.getMessage();
     }
+
+    @ExceptionHandler(LoanNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String loanNotFoundHandler(LoanNotFoundException loan) {
+        return loan.getMessage();
+    }
+
+    @ExceptionHandler(LoanItemNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String loanItemNotFoundHandler(LoanItemNotFoundException loanItem) {
+        return loanItem.getMessage();
+    }
+
+    @ExceptionHandler(ReserveNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String reserveNotFoundHandler(ReserveNotFoundException reserve) {
+        return reserve.getMessage();
+    }
+    
 }

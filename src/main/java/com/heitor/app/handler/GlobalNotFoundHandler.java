b38dac71db873f9377698ee6fc94483cb89543ec
HeitorDalla <heitorvillavilla@gmail.com>
2026-03-25 09:00:31 -0300

    feat: mudar para um JSON a resposta de um erro no cliente

--- src/main/java/com/heitor/app/handler/GlobalNotFoundHandler.java
+++ src/main/java/com/heitor/app/handler/GlobalNotFoundHandler.java
@@ -1,42 +1,30 @@
 package com.heitor.app.handler;
 
+import com.heitor.app.dto.common.ErroResponseDTO;
 import com.heitor.app.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice // indica que esse advice e inserido diretamente no corpo da resposta (global para tratamento de erro - retorna JSON)
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
 public class GlobalNotFoundHandler {
 
-    @ExceptionHandler(UserNotFoundException.class) // captura excessoes especificas (classe)
-    @ResponseStatus(HttpStatus.NOT_FOUND) // define o status retornado
-    public String userNotFoundHandler(UserNotFoundException user) {
-        return user.getMessage();
-    }
-
-    @ExceptionHandler(BookNotFoundException.class)
+    @ExceptionHandler({
+            UserNotFoundException.class,
+            BookNotFoundException.class,
+            LoanNotFoundException.class,
+            LoanItemNotFoundException.class,
+            ReserveNotFoundException.class
+    })
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String bookNotFoundHandler(BookNotFoundException book) {
-        return book.getMessage();
+    public ErroResponseDTO handleNotFound(RuntimeException ex) {
+        return new ErroResponseDTO(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage()
+        );
     }
-
-    @ExceptionHandler(LoanNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String loanNotFoundHandler(LoanNotFoundException loan) {
-        return loan.getMessage();
-    }
-
-    @ExceptionHandler(LoanItemNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String loanItemNotFoundHandler(LoanItemNotFoundException loanItem) {
-        return loanItem.getMessage();
-    }
-
-    @ExceptionHandler(ReserveNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String reserveNotFoundHandler(ReserveNotFoundException reserve) {
-        return reserve.getMessage();
-    }
-    
 }
